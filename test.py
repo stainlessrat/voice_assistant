dba23c24516d73c1f42f0959143b049bf4572657
Sergey Stroev <stainlessrat83@gmail.com>
2021-10-30 21:30:26 +0700

    Добавил распознование голоса

--- test.py
+++ test.py
@@ -2,26 +2,38 @@ from gtts import gTTS
 import random
 import time
 import playsound
+import speech_recognition as sr
 
 
 def listen_command():
-    return input("Скажите вашу команду: ")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Скажите что-нибудь:')
+        audio = r.listen(source)
+    try:
+        our_speech = r.recognize_google(audio, language='ru')
+        print(our_speech)
+        return our_speech
+    except sr.UnknownValueError:
+        return 'Google Speech Recognition could not undestand audio'
+    except sr.RequestError as e:
+        return 'Could not requests from Google Speech Recognition service {e}'
 
 
 def do_this_command(message):
     message = message.lower()
     if 'привет' in message:
-        say_message('Привет, друг.')
+        say_message('Привет друг')
     elif 'пока' in message:
-        say_message('Пока!')
+        say_message('Пока')
         exit()
     else:
-        say_message('Команда не распознана!')
+        say_message('Команда не распознана')
 
 
 def say_message(message):
     voice = gTTS(message, lang='ru')
-    file_voice_name = f'_audio_{str(time.time())}_{str(random.randint(0, 100000))}.mp3'
+    file_voice_name = f'_audio_{str(int(time.time()))}_{str(random.randint(0, 100000))}.mp3'
     voice.save(file_voice_name)
     playsound.playsound(file_voice_name)
     print(f'Голосовой ассистент: {message}')
--- venv/Scripts/pipwin-script.py
+++ venv/Scripts/pipwin-script.py
@@ -1,6 +1,6 @@
 #!D:\Python\voice_assistant\venv\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'pipwin==0.4.6','console_scripts','pipwin'
-__requires__ = 'pipwin==0.4.6'
+# EASY-INSTALL-ENTRY-SCRIPT: 'pipwin==0.5.1','console_scripts','pipwin'
+__requires__ = 'pipwin==0.5.1'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('pipwin==0.4.6', 'console_scripts', 'pipwin')()
+        load_entry_point('pipwin==0.5.1', 'console_scripts', 'pipwin')()
     )
